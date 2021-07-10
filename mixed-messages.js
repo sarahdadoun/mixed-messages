276251764517378b1663eb6945e7f23751dbca85
sdadoun <sarah.dadoun@hotmail.com>
2021-07-10 16:46:42 -0400

    added the first comment

---
+++ mixed-messages.js
@@ -0,0 +1,9 @@
+/*  
+    
+    Mixed messages is a program that prints out random messages 
+    from the tv show Friends.
+    
+    Everytime the program prints a new message, it will prompt 
+    the user for a response to either print a new one or not.
+    
+*/
